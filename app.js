0409bf306be5139b36bbbf3006130a76d224a41a
fabiomazzilli85 <info@fabiomazzilli.com>
2024-10-16 13:29:25 +0200

    ADD HTML, CSS and JS files

---
+++ app.js
@@ -0,0 +1,35 @@
+
+const dipendenti = [
+    { "nome": "Mario", "cognome": "Rossi", "età": 30, "professione": "Ingegnere" },
+    { "nome": "Luca", "cognome": "Bianchi", "età": 25, "professione": "Medico" },
+    { "nome": "Giulia", "cognome": "Verdi", "età": 28, "professione": "Avvocato" },
+    { "nome": "Anna", "cognome": "Neri", "età": 32, "professione": "Insegnante" },
+    { "nome": "Federico", "cognome": "Gialli", "età": 27, "professione": "Architetto" },
+    { "nome": "Sara", "cognome": "Rossi", "età": 29, "professione": "Designer" },
+    { "nome": "Giovanni", "cognome": "Blu", "età": 35, "professione": "Chef" },
+    { "nome": "Elena", "cognome": "Arancioni", "età": 26, "professione": "Fotografa" },
+    { "nome": "Marta", "cognome": "Grigi", "età": 31, "professione": "Scrittrice" },
+    { "nome": "Paolo", "cognome": "Viola", "età": 33, "professione": "Programmatore" },
+    { "nome": "Gianni", "cognome": "Azzurri", "età": 23, "professione": "Copywriter" },
+    { "nome": "Silvio", "cognome": "Beisoldoni", "età": 87, "professione": "CEO" }
+  ];
+  
+  function createCard(dipendenti) {
+    return `
+      <div class="col-md-4">
+        <div class="card mb-4 my-bg">
+          <div class="card-body">
+            <h5 class="text-center mb-3">${dipendenti.nome} ${dipendenti.cognome}</h5>
+            <p class="card-text">Età: ${dipendenti.età}</p>
+            <p class="card-text">Professione: ${dipendenti.professione}</p>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+  
+  const container = document.getElementById('card-container');
+  dipendenti.forEach(dipendenti => {
+    container.innerHTML += createCard(dipendenti);
+  });
+  
